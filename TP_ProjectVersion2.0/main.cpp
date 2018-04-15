b71e24ac7ffce95fd6bf43a9c360f681b180e57e
ILdar Zinatulin <ildarzinatulin@MacBook-Air-ILdar.local>
2018-04-15 20:31:01 +0300

    Initial Commit

---
+++ TP_ProjectVersion2.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TP_ProjectVersion2.0
+//
+//  Created by ILdar Zinatulin on 15.04.18.
+//  Copyright © 2018 ILdar Zinatulin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
